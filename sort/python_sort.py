c42ba9c904052cba20bd5a9fbefb22c30aa36e7a
joy-ko <joy_ko@42maru.com>
2022-05-23 22:25:30 +0900

    [220523] sort: python sort

---
+++ sort/python_sort.py
@@ -0,0 +1,15 @@
+# basic python sorts
+
+# 1. normal num sort (Big O: nlogn)
+num_list = [333, 24, 154, 4, 72, 2323, 23, 299, 0, 25]
+num_list.sort()
+
+for num in num_list:
+    print(num)
+
+# 2. reversed sort
+num_list.reverse()
+
+for num in num_list:
+    print(num)
+
