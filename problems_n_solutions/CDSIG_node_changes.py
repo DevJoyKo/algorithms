f693eba0c4072edcf4b84a54988d0fdbcbf26c4b
DEVJOYKO <dev.joy.soyoung@gmail.com>
2023-05-21 17:50:31 +0900

    :sparkles: node info change

---
+++ problems_n_solutions/CDSIG_node_changes.py
@@ -0,0 +1,33 @@
+def solution(roadRegister):
+    road_before = {}
+
+    for row_index, row in enumerate(roadRegister):
+        road_before[row_index] = [j for j, connect in enumerate(row) if connect == True]
+
+    road_after = {}
+
+    for node in road_before.keys():
+        if node == len(roadRegister) - 1:
+            new_id = 0
+        else:
+            new_id = node + 1
+        road_after[new_id] = []
+        for connect in road_before[node]:
+            if connect != len(roadRegister) - 1:
+                new_item_id = connect + 1
+            else:
+                new_item_id = 0
+            road_after[new_id] += [new_item_id]
+
+    new_roadRegister = []
+
+    for num, _ in enumerate(roadRegister):
+        num_road = []
+        for j in range(len(roadRegister)):
+            if j in road_after[num]:
+                num_road += [True]
+            else:
+                num_road += [False]
+
+        new_roadRegister.append(num_road)
+    return new_roadRegister
