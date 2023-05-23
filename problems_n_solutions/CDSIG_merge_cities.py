ccaaa755d5ac20787048f3707c0d65f7e32b522a
DEVJOYKO <dev.joy.soyoung@gmail.com>
2023-05-23 23:06:07 +0900

    :sparkles: CDSIG merge cities

--- problems_n_solutions/CDSIG_merge_cities.py
+++ problems_n_solutions/CDSIG_merge_cities.py
@@ -3,6 +3,10 @@ import numpy as np
 # TODO & 분석
 def solution(reg):
     num, city2merge, reg = len(reg), [], np.array(reg)
+    # print(num - num % 2)
+    # for i in range(0, num - num % 2, 2):
+        # print(i)
+
     for c1, c2 in ((i, i + 1) for i in range(0, num - num % 2, 2)):
         if reg[c1][c2]:
             city2merge.append(c2)
@@ -13,3 +17,13 @@ def solution(reg):
     reg = np.delete(reg, city2merge, 1)
 
     return reg
+
+solution([
+  [False, True,  True,  False, False, False, True],
+  [True,  False, True,  False, True,  False, False],
+  [True,  True,  False, True,  False, False, True],
+  [False, False, True,  False, False, True,  True],
+  [False, True,  False, False, False, False, False],
+  [False, False, False, True,  False, False, False],
+  [True,  False, True,  True,  False, False, False]
+])
